d8fa68836c9e32f0fb566e3a77950b27e765e9a9
Rabbidking <bickel.john@yahoo.com>
2025-03-18 18:50:45 -0400

    added cell and maze classes

---
+++ cell.py
@@ -0,0 +1,58 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+
+
+        self._win = win #access to the window
+
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self, x1, y1, x2, y2):
+
+        if self._win is None:
+            return
+
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self.x2 - self.x1) // 2
+        x_center = half_length + self.x1
+        y_center = half_length + self.y1
+
+        half_length2 = abs(to_cell.x2 - to_cell.x1) // 2
+        x_center2 = half_length2 + to_cell.x1
+        y_center2 = half_length2 + to_cell.y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from graphics import Window, Line, Point
+from graphics import *
+from maze import Maze
 
 
 def main():
-    #creates a window, draws some lines, and waits for the window to close
-    win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    l2 = Line(Point(100, 250), Point(300, 100))
-    win.draw_line(l, "black")
-    win.draw_line(l2, "red")
+    
+    num_rows = 12
+    num_cols = 20
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 
 
